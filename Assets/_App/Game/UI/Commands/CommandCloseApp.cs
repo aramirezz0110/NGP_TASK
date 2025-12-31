f5083e46fa62993830be4026419a19797c458497
aromramirez <arommramirezz@gmail.com>
2025-12-31 12:57:06 -0400

    Close app command and project setting for build

---
+++ Assets/_App/Game/UI/Commands/CommandCloseApp.cs
@@ -0,0 +1,15 @@
+﻿using _App.Game.Core.Commands;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace _App.Game.UI.Commands
+{
+    public class CommandCloseApp : Command
+    {
+        protected override UniTask ExecuteInternal()
+        {
+            Application.Quit();
+            return UniTask.CompletedTask;
+        }
+    }
+}
